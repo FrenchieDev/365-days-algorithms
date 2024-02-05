70ca1fb7bf69d4569233ad59910e951cdf350b4f
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-05 16:07:56 -0400

    Solution for compare-scores

--- compare-scores.js
+++ compare-scores.js
@@ -1,5 +1,5 @@
-// Frenchie and Kira each aplyy a job. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: code clarity, originality, and performance.
-// The rating for Frenchie's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Kira's challenge is the triplet b = (b[0], b[1], b[2]).
+// Alice and Bob each aplyy a job. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: code clarity, originality, and performance.
+// The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
 // The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
 
 // SCORE TABLE
@@ -9,3 +9,23 @@
 // Comparison points is the total points a person earned.
 
 // Given a and b, determine their respective comparison points.
+const a = [3,2,1]
+const b =  [1,2,3]
+
+function compareTriplets(a, b) {
+    let aFinal = 0;
+    let bFinal = 0;
+    
+    for (let i= 0; i < a.length; i++) {
+        if (a[i] > b[i]) {
+            aFinal++
+        }  else if (a[i] < b[i]) {
+            bFinal++ 
+        } else {
+            
+        } 
+    }
+    return [aFinal, bFinal]
+}
+
+console.log(compareTriplets(a,b))
