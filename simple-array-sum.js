7f59bc7c5845def3375c6c782fac493e93472060
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-05 15:36:36 -0400

    Solution for simple-array-sum

--- simple-array-sum.js
+++ simple-array-sum.js
@@ -1,2 +1,14 @@
 //  EXPLANATION
 // Create a function "simpleArraySum". It must return the sum of the array elements as an integer.
+
+const arr = [ 1, 6, 9, 4 ]
+
+function simpleArraySum(ar) {
+    let result = 0
+    for (let i = 0; i < ar.length; i++){
+        result = result + ar[i];
+    } 
+return result
+}
+
+console.log(simpleArraySum(arr))
