9e393e5437e58c89f76d4ab2c481daeef38e61f7
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-07 20:48:38 -0400

    Solved: A very big Sum

---
+++ very-big-sum.js
@@ -0,0 +1,13 @@
+// Create "aVeryBigSum" function. It must return the sum of all array elements.
+
+
+const a = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+
+function aVeryBigSum(ar) {
+    let result = 0
+    for (let i = 0; i < ar.length; i++)
+        result = result + ar[i]
+        return result
+}
+
+console.log(aVeryBigSum(a))
