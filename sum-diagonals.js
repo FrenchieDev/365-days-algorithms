393e05c876644b629f934af57dcf72e894180cf3
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-07 22:07:52 -0400

    Challenge: Solved sum diagonals

--- sum-diagonals.js
+++ sum-diagonals.js
@@ -1,1 +1,34 @@
 // Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+const m = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+const n = [
+    [11, 2, 4],
+    [4, 5, 6],
+    [10, 8, -12]
+]
+
+function diagonalDifference(arr) {
+    let leftDiagonal = 0;
+    let rightDiagonal = 0;
+    let result = 0
+    
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (i === j) {
+                leftDiagonal = leftDiagonal + arr[i][j]
+            }
+            if (i + j === arr.length - 1) {
+                rightDiagonal = rightDiagonal + arr[i][j]
+            }
+        }
+    }
+    return result = (leftDiagonal - rightDiagonal) * -1
+}
+
+console.log(diagonalDifference(m))
+console.log(diagonalDifference(n))
