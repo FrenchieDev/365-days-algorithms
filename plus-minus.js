a5789af9b5b8ae24672a0cde37bde4b9ac136b54
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-09 13:22:34 -0400

    Challenge: Solved plus-minus

--- plus-minus.js
+++ plus-minus.js
@@ -1,27 +1,43 @@
-// Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+// Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with places after the decimal.
 
 const arr = [ -1, 1, -1, 1, 0 ]
 
 function plusMinus(arr) {
     let total0 = 0
-    let totalpositive = 0
-    let totalnegative = 0
+    let totalPositive = 0
+    let totalNegative = 0
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === 0) {
             total0++
         }
         if (arr[i] > 0) {
-            totalpositive++
+            totalPositive++
         }
         if (arr[i] < 0) {
-            totalnegative++
+            totalNegative++
         }
     }
     
     let final0 = total0 / arr.length
-    let finalPositive = totalpositive / arr.length
-    let finalNegative = totalnegative / arr.length
+    let finalPositive = totalPositive / arr.length
+    let finalNegative = totalNegative / arr.length
     
+    final0 = final0 + ''
+    finalPositive = finalPositive + ''
+    finalNegative = finalNegative + ''
+    
+    while (final0.length < 8) {
+        final0 = final0 + '0'
+      }
+
+      while (finalPositive.length < 8) {
+        finalPositive = finalPositive + '0'
+      }
+
+      while (finalNegative.length < 8) {
+        finalNegative = finalNegative + '0'
+      }
+
     console.log(final0)
     console.log(finalPositive)
     console.log(finalNegative)
