f482b0255b067a1d7bc50f904675b0b8c9450275
FrenchieDev <FrenchieDeveloper@gmail.com>
2024-02-09 12:47:48 -0400

    Challenge: New plus-minus algorithm

---
+++ plus-minus.js
@@ -0,0 +1,29 @@
+// Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+const arr = [ -1, 1, -1, 1, 0 ]
+
+function plusMinus(arr) {
+    let total0 = 0
+    let totalpositive = 0
+    let totalnegative = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            total0++
+        }
+        if (arr[i] > 0) {
+            totalpositive++
+        }
+        if (arr[i] < 0) {
+            totalnegative++
+        }
+    }
+    
+    let final0 = total0 / arr.length
+    let finalPositive = totalpositive / arr.length
+    let finalNegative = totalnegative / arr.length
+    
+    console.log(final0)
+    console.log(finalPositive)
+    console.log(finalNegative)
+}
+
+plusMinus(arr)
